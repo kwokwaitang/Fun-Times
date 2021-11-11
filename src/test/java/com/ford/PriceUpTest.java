d5696e1066564fdb80b70cda0bcf1e4322452f9f
Kwok Wai Tang <kwok.tang68@gmail.com>
2021-11-11 21:35:51 +0000

    Initial unit tests added; added a .gitignore and unstaged .idea

---
+++ src/test/java/com/ford/PriceUpTest.java
@@ -0,0 +1,30 @@
+package com.ford;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PriceUpTest {
+    @Test
+    @DisplayName("Price up basket with 3 tins and 2 loafs of bread, purchase today")
+    void shouldPriceUp_BasketOf3Tins2LoafsOfBread_BoughtToday() {
+        throw new RuntimeException("To be implemented");
+    }
+
+    @Test
+    @DisplayName("Price up basket with 6 apples and 1 bottle of milk, purchase today")
+    void shouldPriceUp_BasketOf6Apples1Milk_BoughtToday() {
+        throw new RuntimeException("To be implemented");
+    }
+
+    @Test
+    @DisplayName("Price up basket with 6 apples and 1 bottle of milk, to be purchased in 5 days time")
+    void shouldPriceUp_BasketOf6Apples1Milk_BoughtIn5DaysTime() {
+        throw new RuntimeException("To be implemented");
+    }
+
+    @Test
+    @DisplayName("Price up basket with 3 apples, 2 tins of soup and 1 loaf of bread, to be purchased in 5 days time")
+    void shouldPriceUp_BasketOf3Apples2Tins1LoafOfBread_BoughtIn5DaysTime() {
+        throw new RuntimeException("To be implemented");
+    }
+}
