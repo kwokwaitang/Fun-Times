10b4772ecffe7665a61b584ede6870aaaecbe552
Kwok Wai Tang <kwok.tang68@gmail.com>
2024-06-25 15:45:36 +0100

    refactor: created EMPTY_BASKET externally of the assertThrows to reduce the number of instantiations; using string representations for BigDecimals; change from assertTrue to assertEquals

--- src/test/java/com/ford/PriceUpTest.java
+++ src/test/java/com/ford/PriceUpTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -17,6 +16,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class PriceUpTest {
+
+    private static final List<StockItem> EMPTY_BASKET = Collections.emptyList();
 
     @Test
     @DisplayName("When the basket of goods is unavailable")
@@ -35,7 +36,7 @@ class PriceUpTest {
     @DisplayName("When the basket of goods is empty")
     void constructorWithMissingBasket2() {
         Exception exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            new HenryGroceryBasket(Collections.emptyList(), PURCHASE_TODAY);
+            new HenryGroceryBasket(EMPTY_BASKET, PURCHASE_TODAY);
         });
 
         String expectedMessage = "Received empty basket";
@@ -47,10 +48,8 @@ class PriceUpTest {
     @Test
     @DisplayName("When the date of purchase is missing")
     void constructorWithMissingDateOfPurchase() {
-        List<StockItem> emptyBasket = new ArrayList<>();
-
         Exception exception = Assertions.assertThrows(NullPointerException.class, () -> {
-            new HenryGroceryBasket(emptyBasket, null);
+            new HenryGroceryBasket(EMPTY_BASKET, null);
         });
 
         String expectedMessage = "Missing a date of purchase";
@@ -63,8 +62,8 @@ class PriceUpTest {
     @DisplayName("Price up basket with 3 tins and 2 loafs of bread, purchase today")
     void shouldPriceUp_BasketOf3Tins2LoafsOfBread_BoughtToday() {
         // Given...
-        StockItem soupTins = new StockItem("soup", StockItemType.TIN, new BigDecimal(0.65), 3);
-        StockItem loavesOfBread = new StockItem("bread", StockItemType.LOAF, new BigDecimal(0.80), 2);
+        StockItem soupTins = new StockItem("soup", StockItemType.TIN, new BigDecimal("0.65"), 3);
+        StockItem loavesOfBread = new StockItem("bread", StockItemType.LOAF, new BigDecimal("0.80"), 2);
         List<StockItem> basket = new ArrayList<>(Arrays.asList(soupTins, loavesOfBread));
 
         Basket henryGroceryBasket = new HenryGroceryBasket(basket);
@@ -74,15 +73,15 @@ class PriceUpTest {
 
         // Then...
         BigDecimal expected = new BigDecimal("3.15");
-        assertTrue(expected.compareTo(grandTotal) == 0);
+        assertEquals(0, expected.compareTo(grandTotal));
     }
 
     @Test
     @DisplayName("Price up basket with 6 apples and 1 bottle of milk, purchase today")
     void shouldPriceUp_BasketOf6Apples1Milk_BoughtToday() {
         // Given...
-        StockItem apples = new StockItem("apple", StockItemType.SINGLE, new BigDecimal(0.10), 6);
-        StockItem bottleOfMilk = new StockItem("milk", StockItemType.BOTTLE, new BigDecimal(1.30), 1);
+        StockItem apples = new StockItem("apple", StockItemType.SINGLE, new BigDecimal("0.10"), 6);
+        StockItem bottleOfMilk = new StockItem("milk", StockItemType.BOTTLE, new BigDecimal("1.30"), 1);
         List<StockItem> basket = new ArrayList<>(Arrays.asList(apples, bottleOfMilk));
 
         Basket henryGroceryBasket = new HenryGroceryBasket(basket, PURCHASE_TODAY);
@@ -100,8 +99,8 @@ class PriceUpTest {
     @DisplayName("Price up basket with 6 apples and 1 bottle of milk, to be purchased in 5 days time")
     void shouldPriceUp_BasketOf6Apples1Milk_BoughtIn5DaysTime() {
         // Given...
-        StockItem apples = new StockItem("apple", StockItemType.SINGLE, new BigDecimal(0.10), 6);
-        StockItem bottleOfMilk = new StockItem("milk", StockItemType.BOTTLE, new BigDecimal(1.30), 1);
+        StockItem apples = new StockItem("apple", StockItemType.SINGLE, new BigDecimal("0.10"), 6);
+        StockItem bottleOfMilk = new StockItem("milk", StockItemType.BOTTLE, new BigDecimal("1.30"), 1);
         List<StockItem> basket = new ArrayList<>(Arrays.asList(apples, bottleOfMilk));
 
         Basket henryGroceryBasket = new HenryGroceryBasket(basket, PURCHASE_IN_FIVE_DAYS_TIME);
@@ -119,9 +118,9 @@ class PriceUpTest {
     @DisplayName("Price up basket with 3 apples, 2 tins of soup and 1 loaf of bread, to be purchased in 5 days time")
     void shouldPriceUp_BasketOf3Apples2Tins1LoafOfBread_BoughtIn5DaysTime() {
         // Given...
-        StockItem apples = new StockItem("apple", StockItemType.SINGLE, new BigDecimal(0.10), 3);
-        StockItem soupTins = new StockItem("soup", StockItemType.TIN, new BigDecimal(0.65), 2);
-        StockItem loafOfBread = new StockItem("bread", StockItemType.LOAF, new BigDecimal(0.80), 1);
+        StockItem apples = new StockItem("apple", StockItemType.SINGLE, new BigDecimal("0.10"), 3);
+        StockItem soupTins = new StockItem("soup", StockItemType.TIN, new BigDecimal("0.65"), 2);
+        StockItem loafOfBread = new StockItem("bread", StockItemType.LOAF, new BigDecimal("0.80"), 1);
         List<StockItem> basket = new ArrayList<>(Arrays.asList(apples, soupTins, loafOfBread));
 
         Basket henryGroceryBasket = new HenryGroceryBasket(basket, PURCHASE_IN_FIVE_DAYS_TIME);
